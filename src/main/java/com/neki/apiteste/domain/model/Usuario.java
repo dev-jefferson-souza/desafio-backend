3233bcb81538ef1910daa1c8c8e788d583348b05
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-03-08 12:40:50 -0300

    feat(Model): criação do modelo de Usuario

---
+++ src/main/java/com/neki/apiteste/domain/model/Usuario.java
@@ -0,0 +1,67 @@
+package com.neki.apiteste.domain.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SequenceGenerator(
+  name = "generator_user",
+  sequenceName = "user_seq",
+  initialValue = 1000,
+  allocationSize = 1,
+  schema = "teste_residencia"
+)
+
+@Entity(name = "Usuario")
+@Table(name = "user", schema = "teste_residencia")
+public class Usuario {
+
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "generator_user"
+  )
+  private Long id;
+
+   @Column(nullable = false, length = 12)
+  private String login;
+
+  // @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  @Column(nullable = false, length = 100)
+  private String password;
+
+  @UpdateTimestamp
+  @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+  @Column(name = "last_login_date")
+  private Date lastLoginDate;
+
+  @ManyToMany
+  @JoinTable(
+    name = "userSkill",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "skill_id")
+  )
+  private List<Skill> skill = new ArrayList<Skill>();
+}
